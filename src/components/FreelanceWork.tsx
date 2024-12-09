a80fb3692dcbc1ce0e8f9e1562ce538943cb1f0b
syd enciso <encisosydney19@gmail.com>
2024-12-09 12:07:19 +0800

    portfolio

---
+++ src/components/FreelanceWork.tsx
@@ -0,0 +1,295 @@
+import React from "react";
+
+function FreelanceWork() {
+  return (
+    <>
+      <div>
+        <div className='experience__header'>
+          <div className='experience__company-group'>
+            <img
+              title='Samsung Research & Development'
+              alt='samsung-logo'
+              loading='lazy'
+              src='https://images.samsung.com/is/image/samsung/assets/global/about-us/brand/logo/256_144_4.png?$512_N_PNG$'
+            />
+          </div>
+          <div className='experience__company-name'>
+            Samsung Research & Development |{" "}
+            <span className='experience__position'>Software Engineer II</span>
+          </div>
+        </div>
+        <div className='experience__details'>
+          <ul>
+            <li>Associate Developer for Knox Project.</li>
+            <li>
+              Lead a Knox Project service Knox Mobile Enrollment, Implementing
+              Revamp design, Optimizing performance, Enhancing API Security,
+              Providing E2E Support.
+            </li>
+            <li>
+              Lead a Knox Project service Knox Configure, Implementing Revamp
+              design, Optimizing performance, Enhancing API Security, Providing
+              E2E Support.
+            </li>
+            <li>
+              Associate Developer for Samsung Galaxy S24 AI Camera & Photo
+              Enhancement
+            </li>
+            <li>Resolve Bugs & Production issues.</li>
+            <li>Facilitate Deployment.</li>
+            <li>Facilitate Code Merging to Develop/QA/Production Brances.</li>
+            <li>Participate in Code Review.</li>
+            <img
+              title='Knox'
+              alt='knox-branding'
+              className='experience__details-img'
+              src='https://www.red-dot.org/fileadmin/_processed_/7/d/csm_05856-2023BC.1003713.org_87107c17c1.jpg'
+            />
+            <img
+              title='Knox Mobile Enrollment'
+              alt='kme-branding'
+              className='experience__details-img'
+              src='https://kp4-cdn.samsungknox.com/img/KME_Section%2002_-HuX.png'
+            />
+            <img
+              title='Knox Configure'
+              alt='kc-branding'
+              className='experience__details-img'
+              src='https://i.ytimg.com/vi/ec0-F8leLGc/maxresdefault.jpg'
+            />
+          </ul>
+        </div>
+
+        <div className='experience__header'>
+          <div className='experience__company-group'>
+            <img
+              title='Collabera Digital'
+              alt='collabera-logo'
+              loading='lazy'
+              src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqQOiujYP0MGgb3h5ExHZnGl25ryFKigyiJg&s'
+            />
+            <img
+              title='Singlife Philippines'
+              alt='singlife-logo'
+              loading='lazy'
+              src='https://play-lh.googleusercontent.com/929IyF8o_m1nlycM1Wy-Uu34vl4X8aqfMcLN9Wjoil85Ao6zfge4l416GGBT5Msdelg'
+            />
+          </div>
+          <div className='experience__company-name'>
+            Collabera Digital / Singlife Philippines |{" "}
+            <span className='experience__position'>ReactJS Engineer</span>
+          </div>
+        </div>
+        <div className='experience__details'>
+          <ul>
+            <li>
+              Integrate new features for Singlife Micro Web app with GCash
+            </li>
+            <li>
+              Handle Singlife Product Cash for Goals, Implementing Performance,
+              Security Enhance.
+            </li>
+            <li>
+              Handle Singlife Product Cash for Income Loss, Implementing new
+              features introduced and provide E2E support for production
+              deployment.
+            </li>
+            <li>Associate Developer for Moovr Application</li>
+            <li>Resolve Bugs & Production issues.</li>
+            <li>Facilitate Deployment.</li>
+            <li>Facilitate Code Merging to Develop/QA/Production Brances.</li>
+            <li>Participate in Code Review.</li>
+            <img
+              title='Cash for Goals'
+              alt='cfg-branding'
+              className='experience__details-img'
+              src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf1cFXqIm59BM8HV4q2nIFw4XNxptyLFgcqg&s'
+            />
+            <img
+              title='Cash for Income Loss'
+              alt='cfil-branding'
+              className='experience__details-img'
+              src='https://media.philstar.com/photos/2022/11/17/singlife-gcash_2022-11-17_10-58-35.jpg'
+            />
+            <img
+              title='Moovr'
+              alt='moovr-branding'
+              className='experience__details-img'
+              src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs3AutZxx3DUsiqRKQr_kJtQGy7np2GOwLkA&s'
+            />
+          </ul>
+        </div>
+
+        <div className='experience__header'>
+          <div className='experience__company-group'>
+            <img
+              title='Chenvel Services Inc'
+              alt='chenvel-logo'
+              loading='lazy'
+              src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1_OEKke6nG0p3g5C1-AALgNFtPdOduiu4Mg&s'
+            />
+          </div>
+          <div className='experience__company-name'>
+            Chenvel Services Inc |{" "}
+            <span className='experience__position'>
+              Full-Stack .Net Developer
+            </span>
+          </div>
+        </div>
+        <div className='experience__details'>
+          <ul>
+            <li>
+              Associate for platformization of Chenvel Services Inc Legacy
+              system to modern framework such as ReactJS, ExpressJS.
+            </li>
+            <li>Developed a Parcel Tracking website.</li>
+            <li>Developed a Warehouse Parcel Positioning.</li>
+            <li>Developed Accounting System for HR.</li>
+            <li>
+              Developed a Queueing Encoding System for Packinglist and Emptybox
+            </li>
+            <li>Developed a Real-Time Statistics report of encoders.</li>
+            <li>Developed a Queueing quality checking system.</li>
+            <li>Developed a Parcel barcode encoding.</li>
+            <li>Resolve Bugs & Production issues.</li>
+            <li>Facilitate Deployment.</li>
+            <li>Facilitate Code Merging to Develop/QA/Production Brances.</li>
+            <li>Participate in Code Review.</li>
+          </ul>
+        </div>
+
+        <div className='experience__header'>
+          <div className='experience__company-group'>
+            <img
+              title='Petsure Services Inc'
+              alt='petsure-logo'
+              loading='lazy'
+              src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuw7munY42233xJmNCTehBo7NrJNg0DK_RTA&s'
+            />
+          </div>
+          <div className='experience__company-name'>
+            Petsure Services Inc |{" "}
+            <span className='experience__position'>Claims Admin</span>
+          </div>
+        </div>
+        <div className='experience__details'>
+          <ul>
+            <li>Encode filed insurance and assess for qualification.</li>
+            <li>
+              Review Veterinary clinic report for insurance qualification.
+            </li>
+          </ul>
+        </div>
+
+        <div className='experience__header'>
+          <div className='experience__company-group'>
+            <img
+              title='Philippine Statistics Authority'
+              alt='PSA-logo'
+              loading='lazy'
+              src='https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgD1s21ML55b08z-QDtvS8GuQPXWlkU6LxhoVPT0evOLp_8iBY8YFGmnoaiUOjaxrzkx7U30aPMTRt9rcDBFwoOewEnVlFAbvkNY7A1CuaA3-d-FMUNLU-_yB8fMkd_dQjpoixX568092RvvywoUrd9tkN0pVVA8Z9GVUMZKpNY2t2AUoGCQLXZaOD9Ey6F/s800/psa-logo-ph-1.png'
+            />
+          </div>
+          <div className='experience__company-name'>
+            Philippine Statistics Authority |{" "}
+            <span className='experience__position'>Data Encoder</span>
+          </div>
+        </div>
+        <div className='experience__details'>
+          <ul>
+            <li>Encode 2012-2014 data census.</li>
+            <li>Generate statistics report for 2012-2014 data.</li>
+          </ul>
+        </div>
+
+        <div className='experience__header'>
+          <div className='experience__company-group'>
+            <img
+              title='FAD Modeling Center'
+              alt='FAD-logo'
+              loading='lazy'
+              src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRubacMJyxenF7ZE-LAQ-HEvkGHoKTksAt67Q&s'
+            />
+          </div>
+          <div className='experience__company-name'>
+            FAD Modeling Center |{" "}
+            <span className='experience__position'>Web Support</span>
+          </div>
+        </div>
+        <div className='experience__details'>
+          <ul>
+            <li>Maintain FAD Modeling Center website by providing contents.</li>
+            <li>
+              Re-design FAD Modeling Center website using WordPress Theme
+              Configurator.
+            </li>
+            <li>
+              Provide support for video, photo editing for FAD Facebook Page.
+            </li>
+          </ul>
+        </div>
+
+        <div className='experience__header'>
+          <div className='experience__company-group'>
+            <img
+              title='Freelancer'
+              alt='freelancer-logo'
+              loading='lazy'
+              src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTClYe1kHtbUV-E2d7NuF2pdPxljtu4nlXutg&s'
+            />
+            <img
+              title='Upwork'
+              alt='upwork-logo'
+              loading='lazy'
+              src='https://seeklogo.com/images/U/upwork-logo-38004EEA61-seeklogo.com.png'
+            />
+          </div>
+          <div className='experience__company-name'>
+            Freelancer / Upwork |{" "}
+            <span className='experience__position'>Full-Stack Developer</span>
+          </div>
+        </div>
+        <div className='experience__details'>
+          <ul>
+            <li>
+              Collaborate with different nationality to work with their project.
+            </li>
+            <li>
+              Implement new features for the existing project/repositories.
+            </li>
+            <li>Integrating API related to authentication etc.</li>
+            <li>
+              Develop mobile app & web app from scratch using ReactJS and
+              ExpressJS
+            </li>
+            <li>Provide E2E support for production deployment.</li>
+            <li>Extract PDF data to MSWord file.</li>
+            <li>
+              Support multimedia such as Video, Photo editing and Logo creation.
+            </li>
+            <li>
+              Developed a mobile e-learning app for china using ReactNative.
+            </li>
+            <img
+              title='SpeedUp Learning'
+              alt='speedup-branding'
+              className='experience__details-img'
+              src='https://sydney-enciso.vercel.app/assets/speedup-D7pPCZ1X.png'
+            />
+            <img
+              onClick={() =>
+                window.open("https://tripbuddyai.vercel.app/", "_blank")
+              }
+              title='TripBuddyAI'
+              alt='tripbuddy-branding'
+              className='experience__details-img'
+              src='https://sydney-enciso.vercel.app/assets/tripbuddy-branding-Efq2O5h8.png'
+            />
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default FreelanceWork;
